234f25a076f7a0eddd515bd74ad7692cff9d9468
Desislav Arashev <darashev@cst-bg.net>
2024-07-16 14:22:16 +0300

    Update resolvers schema

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -18,5 +18,8 @@ func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) 
 
 // Todos is the resolver for the todos field.
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+	todos := []*model.Todo{
+		{ID: "1", Text: "todo1", Done: false},
+	}
+	return todos, nil
 }
